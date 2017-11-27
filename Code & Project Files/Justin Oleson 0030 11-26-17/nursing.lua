4e01be1c1c825b004c290c6c86f99f8de39cd9a6
JustinOleson <33971875+JustinOleson@users.noreply.github.com>
2017-11-26 18:04:54 -0600

    t
    t

--- Code & Project Files/Justin Oleson 0030 11-26-17/nursing.lua
+++ Code & Project Files/Justin Oleson 0030 11-26-17/nursing.lua
@@ -1,5 +1,11 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
+
+
+
+
+
+
  
 ---------------------------------------------------------------------------------
 -- All code outside of the listener functions will only be executed ONCE
@@ -25,6 +31,8 @@ end
 function scene:create( event )
  
    local sceneGroup = self.view
+   nurseMusic = audio.loadStream("titleMusic.wav");
+   audio.play(titleMusic, { channel = 2, loops =- 1, fadein = 500 });
  
    -- Initialize the scene here.
    -- Example: add display objects to "sceneGroup", add touch listeners, etc.
@@ -42,15 +50,13 @@ function scene:show( event )
 	local background = display.newImage("nursing.jpg", display.contentCenterX, display.contentCenterY)
 	background:scale(1.27,1.15)
 	local nurse = display.newImage("nursenormal.png",130, 585)
-	local speechbubble = display.newImage("speechbubble.png", 675, 300)
-	speechbubble:scale(1.3,.7)
-	speechbubble.alpha = 0.85
+	
 	--local speechtext= display.newText("Do some fucking work, Hawkins", 492, 838, native.systemFont, 21 )
 	--speechtext:setFillColor(0,0,0)
 	
 	local options = 
 {
-    text = "I’m sorry, I find it very hard to believe that you, a total stranger, legitimately give a s**t about what I’ve got going on right now, and also I’m picking up some serious Weinstein vibes from you right now… Ok look, that was kind of harsh. I tell you what if you can answer this question correctly, which based how big of a dunce you look like asking a random nursing student on her way to class “Do you wanna talk about it” I’d be pretty surprised if you did, MAYBE we can talk about it sometime later.",
+    text = "Why? Why did I ever think that I wanted to become a nurse?! Who said this would be a good idea? My friends tell me that you have to be pretty smart and with it to get through nursing school but I feel more like a moron each day that goes by that I don’t drop out and major in accounting. Or there’s always the Pony…",
     x = 675,
     y = 300,
     width = 500,
@@ -61,6 +67,32 @@ function scene:show( event )
  
 local myText = display.newText( options )
 myText:setFillColor( 0, 0, 0 )
+
+
+
+local speechbubble = display.newImage("speechbubble.png", 675, 300)
+	speechbubble:scale(1.3,.7)
+	speechbubble.alpha = 0.85
+	
+myText:toFront()
+function onPress(event)
+	if (event.phase == "ended") then
+		--display.remove(speechbubble)
+		display.remove(myText)
+		
+		
+		
+		--moreText()
+		
+	end
+end
+speechbubble:addEventListener("touch", onPress)
+
+
+
+
+
+
 	--self.view:insert(nursingbg);
 	--button1 = display.newRect(150,150, 200, 200)
 	--button1:setFillColor(0,0,1)
